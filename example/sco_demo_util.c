01fea8fe71670a2c82445861435ecb6f5c775026
Joep Moritz <joep.moritz@gmail.com>
2016-10-23 13:04:26 +0100

    Disable PLC and route audio in directly back to audio out.

--- example/sco_demo_util.c
+++ example/sco_demo_util.c
@@ -167,14 +167,15 @@ static void sco_demo_init_CVSD(void){
     num_samples_to_write = sample_rate * SCO_WAV_DURATION_IN_SECONDS;
 }
 
+
+int8_t audio_frame_out[24];
 static void sco_demo_receive_CVSD(uint8_t * packet, uint16_t size){
     if (num_samples_to_write){
         const int num_samples = size - 3;
         const int samples_to_write = btstack_min(num_samples, num_samples_to_write);
-        int8_t audio_frame_out[24];
         
-        // memcpy(audio_frame_out, (int8_t*)(packet+3), 24);
-        btstack_cvsd_plc_process_data(&cvsd_plc_state, (int8_t *)(packet+3), num_samples, audio_frame_out);
+        memcpy(audio_frame_out, (int8_t*)(packet+3), 24);
+        // btstack_cvsd_plc_process_data(&cvsd_plc_state, (int8_t *)(packet+3), num_samples, audio_frame_out);
 
         wav_writer_write_int8(samples_to_write, audio_frame_out);
         num_samples_to_write -= samples_to_write;
@@ -306,7 +307,7 @@ void sco_demo_send(hci_con_handle_t sco_handle){
     little_endian_store_16(sco_packet, 0, sco_handle);
     // set len
     sco_packet[2] = sco_payload_length;
-    const int audio_samples_per_packet = sco_payload_length;    // for 8-bit data. for 16-bit data it's /2
+    // const int audio_samples_per_packet = sco_payload_length;    // for 8-bit data. for 16-bit data it's /2
 
 #if SCO_DEMO_MODE == SCO_DEMO_MODE_SINE
     if (negotiated_codec == HFP_CODEC_MSBC){
@@ -322,7 +323,9 @@ void sco_demo_send(hci_con_handle_t sco_handle){
 
         sco_demo_fill_audio_frame();
     } else {
-        wav_synthesize_sine_wave_int8(audio_samples_per_packet, (int8_t *) (sco_packet+3));
+        // wav_synthesize_sine_wave_int8(audio_samples_per_packet, (int8_t *) (sco_packet+3));
+        memcpy((int8_t*)(sco_packet+3), audio_frame_out, 24);
+
     }
 #else
 #if SCO_DEMO_MODE == SCO_DEMO_MODE_ASCII
